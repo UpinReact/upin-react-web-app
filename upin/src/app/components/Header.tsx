679fcb680551f663f4fe51625d03b90fcf869fe6
Carlosalvarez1997 <Carlos.alvarez97.ca@gmail.com>
2024-06-18 20:19:08 -0700

    added the nav bar and the google maps.

---
+++ upin/src/app/components/Header.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Image from 'next/image';
+import upinLogo from "../../../public/Upin White (cutout content logo).png"
+
+const Header = () => {
+  return (
+    <nav className='bg-green-400 flex justify-between items-center w-full shadow-2xl shadow-gray-900 z-30 absolute'>
+        <Image src = {upinLogo} alt = "upin main logo" className='px-2 ml-2 shadow-sm'/>
+    <div className='flex-1'> 
+
+    </div> 
+
+    <div className='flex flex-col items-center ml-[-100px]'>
+
+        <h1 className='font-bold text-4xl text-center mb-3'>Upin</h1>
+        <p className='text-center font-serif'>Create. Join. Connect</p>
+        <ul className='flex justify-between px-4 pt-2'>
+            <li className='px-5 hover:text-blue-500 pb-3'>Log In</li>
+            <li className='px-5 hover:text-blue-500 pb-3'>Sign Up</li>
+        </ul>
+
+    </div>
+
+    <div className='flex-1 flex justify-end'>
+
+    <ul className='flex space-x-4 px-5 mr-12'>
+        <li className='hover:text-blue-500 text-lg  font-mono'>News</li>
+        <li className='hover:text-blue-500 text-lg  font-mono'>About</li>
+        <li className='hover:text-blue-500 text-lg  font-mono'>Team</li>
+    </ul>
+
+    </div>
+
+    </nav>
+  );
+}
+
+export default Header;
+
---
+++ upin/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      'phone': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+      
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    }
+  },
+  plugins: [],
+}
