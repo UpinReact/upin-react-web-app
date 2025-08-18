7d714b796776716a36a81d91c8d341d0f3a751fd
UpinBen <30612894+BenHavis@users.noreply.github.com>
2025-08-18 14:58:03 -0400

    Create config.ts

---
+++ utils/config.ts
@@ -0,0 +1,13 @@
+const colors = {
+  secondaryColor: '#0FBC71',
+  white: '#fff',
+  appleblue: '007AFF',
+  mainColor: '#22DFA7',
+  lightGreen: '#ebfdf7',
+  lightRed: '#ffeae9',
+  darkRed: '#9b160d',
+  yellow: '#eec229',
+  rankedColor: '#dba32a',
+  redPin: '#f86f63',
+  orange: '#e89b46'
+};
